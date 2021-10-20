ef4142e0f26d54cbb58226ec5f3b41bf5e6191a1
Cetacea <2369700690@qq.com>
2021-10-20 16:03:58 +0800

    单文件组件

---
+++ 03.vue_basic/19.单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demo">
+    <h2>名称：{{schoolName}}</h2>
+    <h2>地址：{{address}}</h2>
+    <button @click="show">点我有吃的</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'School',
+  data(){
+    return{
+      schoolName:'咕噜噜',
+      address:'咚咚锵'
+    }
+  },
+  methods: {
+    show(){
+      alert('🍇')
+    }
+  },
+}
+</script>
+
+<style>
+  .demo{
+    background-color: plum;
+  }
+</style>
