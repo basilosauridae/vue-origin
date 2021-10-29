2c0e02d58736b6958e33c47fefe510d24199006a
huying <huying@wondertek.com.cn>
2021-10-29 16:19:14 +0800

    tdolist案例 非常值得学习

---
+++ 04.vue_test/src/components/List.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="todo-main">
+    <Item 
+      v-for="item in todos" 
+      :key="item.id" 
+      :todo="item" 
+      :checkTodo="checkTodo"
+      :delTodo="delTodo"
+    ></Item>
+  </ul>
+</template>
+
+<script>
+import Item from './Item.vue'
+export default {
+  name:'List',
+  components:{
+    Item
+  },
+  props:['todos','checkTodo','delTodo']
+}
+</script>
+
+<style scoped>
+  .todo-main{
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+  .todo-empty{
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
