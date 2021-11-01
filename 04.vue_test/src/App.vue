7e91c6321f96c44702943c606f87375a0f9a823f
huying <huying@wondertek.com.cn>
2021-11-01 13:21:00 +0800

    todolist本地储存的应用（深度监听）

--- 04.vue_test/src/App.vue
+++ 04.vue_test/src/App.vue
@@ -1,120 +1,42 @@
 <template>
-  <div id="root">
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <Top :recevice="recevice"></Top>
+  <div class="app">
+    <h1>{{msg}}</h1>
 
-        <List 
-          :todos="todos" 
-          :checkTodo="checkTodo" 
-          :delTodo="delTodo"
-        ></List>
-
-        <Bottom 
-          :todos="todos" 
-          :checkAllToDo="checkAllToDo"
-          :delAllDone = "delAllDone"
-        ></Bottom>
-      </div>
-    </div>
+    <!-- 通过父组件给子组件传递函数类型的props实现：子给父传递数据 -->
+    <School :getSchoolName="getSchoolName"/>
+    <Student/>
   </div>
 </template>
 
 <script>
-import Top from './components/Top.vue'
-import Bottom from './components/Bottom.vue'
-import List from './components/List.vue'
+import School from './components/School.vue'
+import Student from './components/Student.vue'
 
 export default {
   name:'App',
-  components:{
-    Top,
-    Bottom,
-    List
-  },
   data() {
     return {
-      // 由于todos是其他子组件都在使用的数据，所以放在App中（状态提升）
-      todos:[
-        {id:"001",title:"小屁兜",done:false},
-        {id:"004",title:"放臭屁",done:true},
-        {id:"002",title:"好几天",done:true},
-        {id:"003",title:"没拉屎",done:true},
-      ]
+      msg:'Netease Cloud'
     }
   },
   methods: {
-    /* 添加一个todo */
-    recevice(x){
-      // console.log(x,'x')//函数也可以作为一个数据父传子
-      this.todos.unshift(x)
-    },
-
-    /* 勾选或取消勾选,爷传父，父再传子 */
-    checkTodo(id){
-      this.todos.forEach(item => {
-        if(item.id === id) item.done = !item.done
-      });
-    },
-
-    /* 删除 */
-    delTodo(id){
-      // filter方法不改变原数组 需要重新赋值 过滤不等于选中的id的组成新数组
-      this.todos = this.todos.filter(item => item.id !== id)
-    },
-
-    /* 全选or取消全选 */
-    checkAllToDo(done){
-      this.todos.forEach((item)=>{
-        item.done = done
-      })
-    },
-
-    /* 清除已完成 */
-    delAllDone(){
-      this.todos = this.todos.filter((item)=>{
-        return !item.done
-      })
+    getSchoolName(name){
+      console.log('App收到了School的name',name)
     }
   },
+  components:{
+    School,
+    Student
+  }
 }
 </script>
 
 <style>
-  body{
-    background-color: #fff;
-  }
-  .btn{
-    display: inline-block;
-    padding: 4px 12px;
-    margin-bottom: 0;
-    font-size: 14px;
-    line-height: 20px;
-    text-align: center;
-    vertical-align: middle;
-    cursor: pointer;
-    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),0 1px 2px rgba(0, 0, 0, 0.05);
-    border-radius: 4px;
-  }
-  .btn-danger{
-    color: #fff;
-    background-color: #da4f49;
-    border: 1px solid #bd362f;
-  }
-  .btn-danger:hover{
-    color: #fff;
-    background-color: #bd362f;
-  }
-  .btn:focus{
-    outline: none;
-  }
-  .todo-container{
+  .app{
     width: 600px;
-    margin:0 auto
-  }
-  .todo-container .todo-wrap{
+    color: #fff;
     padding: 10px;
-    border:1px solid #ddd;
-    border-radius: 5px;
+    text-align: center;
+    background-color: #2e2b2a;
   }
 </style>
