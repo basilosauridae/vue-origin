c157b53345043f71b9f654958d3b4400e877c534
New Author <2369700690@qq.com>
2021-11-04 10:32:22 +0800

    todoList中的自定义事件

---
+++ 04.vue_test/src/components/Student.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="demo">
+    <h3>姓名：{{nickName}}</h3>
+    <h3>性别：{{sex}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Student',
+  data() {
+    return {
+      nickName:'小黄',
+      sex:'gender'
+    }
+  }
+
+}
+</script>
+// 若不写lang,则默认lang="css"
+<style scoped>
+  .demo{
+    background-color: skyblue;
+  }
+  
+</style>
