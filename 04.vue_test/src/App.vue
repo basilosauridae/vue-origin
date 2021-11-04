ac368fd4d4dae644a9b9b43bf1d5bd4313385bad
huying <huying@wondertek.com.cn>
2021-11-04 10:32:22 +0800

    todoList中的自定义事件

--- 04.vue_test/src/App.vue
+++ 04.vue_test/src/App.vue
@@ -1,62 +1,23 @@
 <template>
-  <div class="app">
-    <h1>{{msg}}</h1>
-
-    <!-- 通过父组件给子组件传递函数类型的props实现：子给父传递数据 -->
-    <Studio :getStudiolName="getStudiolName"/>
-
-    <!-- 传统父传子：通过父组件给子组件绑定一个自定义事件实现 -->
-    <Singer @nima="demo" @click.native="origin"/>
-
-    <!-- ref是方法三，灵活性强，需求是过一段时间再触发自定义事件  -->
-    <!-- <Singer ref="singer"/> -->
+  <div>
+    <School/>
+    <Student/>
   </div>
 </template>
 
 <script>
-import Singer from './components/Singer.vue'
-import Studio from './components/Studio.vue'
+import School from './components/School.vue'
+import Student from './components/Student.vue'
 
 export default {
   name:'App',
-  data() {
-    return {
-      msg:'Netease Cloud'
-    }  
-  },
-  methods: {
-    getStudiolName(name){
-      console.log('App收到了工作室的名称',name)
-    },
-    demo(name,...params){
-      console.log('App收到音乐人的名字',name,params)
-    },
-    origin(){
-      alert('添加native触发组件的原生事件')
-    }
-  },
-  mounted() {
-    /* 像有延时获取需求的可以结合$refs和$on */
-    // setTimeout(()=>{
-    //   this.$refs.singer.$on('nima',this.getStudiolName)
-    // },3000)
-
-    /* 自定义事件触发一次就不触发了 */
-    // this.$refs.singer.$once('nima',this.getStudiolName)
-  },
   components:{
-    Singer,
-    Studio
+    School,
+    Student
   }
 }
 </script>
 
 <style>
-  .app{
-    color: #fff;
-    width: 650px;
-    padding: 10px;
-    text-align: center;
-    background-color: #2e2b2a;
-  }
+
 </style>
---
+++ 04.vue_test/src/components/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo">
+    <h3>名称：{{name}}</h3>
+    <h3 class="add">地址：{{address}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'School',
+  data() {
+    return {
+      name:'网易1912街',
+      address:'上海'
+    }
+  }
+
+}
+</script>
+<style scoped>
+  .demo{
+    background-color: palevioletred;
+    /* .add{
+      font-size: 20px;
+    } */
+  }
+</style>
