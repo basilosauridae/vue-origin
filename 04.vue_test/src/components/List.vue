d2d6d447a4d27d05233266a1e0bcc8087c995912
huying <huying@wondertek.com.cn>
2021-11-12 10:33:27 +0800

    todoList中的动画

---
+++ 04.vue_test/src/components/List.vue
@@ -0,0 +1,53 @@
+<template>
+  <ul class="todo-main">
+    <transition-group name="todo" appear>
+      <Item 
+        v-for="item in todos" 
+        :key="item.id" 
+        :todo="item" 
+      ></Item>
+    </transition-group>
+  </ul>
+</template>
+
+<script>
+import Item from './Item.vue'
+export default {
+  name:'List',
+  components:{
+    Item
+  },
+  props:['todos']
+}
+</script>
+
+<style scoped>
+  .todo-main{
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+  .todo-empty{
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+   .todo-enter-active{
+    animation: fly 0.5s linear;
+  }
+  .todo-leave-active{
+    animation: fly 0.5s linear reverse;
+  }
+  @keyframes fly{
+    from {
+      transform: translateX(100%);
+    }
+    to{
+      transform: translateX(0);
+    }
+  }
+</style>
