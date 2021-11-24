b0947e16172cb8f32f24f5005c59b9eb8015a47e
huying <huying@wondertek.com.cn>
2021-11-24 17:06:37 +0800

    vuex基础写法

--- 04.vue_test/src/store/index.js
+++ 04.vue_test/src/store/index.js
@@ -5,13 +5,46 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 // 准备state 用于存储数据
-const state = {}
+const state = {
+  sum: 0 //当前的和
+}
 
-// 准备mutations 用于操作state中的数据
-const mutations = {}
+// 准备mutations 用于操作state中的数据,不要写任何业务逻辑和ajax请求
+const mutations = {
+  Add(state,value){
+    // console.log('mutations中被调用',state,value)
+    state.sum += value
+  },
+  Minus(state,value){
+    state.sum -= value
+  },
+  Odd(state,value){
+    console.log('Odd',state,value)
+  }
+}
 
 // 准备actions 用于响应组件中的动作
-const actions = {}
+const actions = {
+  /* 没什么逻辑的不需要在actions中再写一次 */
+  /*  add(context,value){
+    console.log('actions中被调用', context, value)
+    context.commit('Add',value)
+  }, */
+  /* minus(context,value){
+    context.commit('Minus',value)
+  }, */
+  odd(context,value){
+    // console.log('odd',context,value)
+    if(context.state.sum % 2){
+      context.commit('Add',value)
+    }
+  },
+  wait(context,value){
+    setTimeout(()=>{
+      context.commit('Add',value)
+    },5000)
+  }
+}
 
 
 // 创建并暴露导出store
