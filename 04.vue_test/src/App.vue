1a44be9c91ac089840ae2fc87d70749debb5e1f8
New Author <2369700690@qq.com>
2021-11-24 09:57:28 +0800

    纯vue实现的求和案例

--- 04.vue_test/src/App.vue
+++ 04.vue_test/src/App.vue
@@ -1,50 +1,19 @@
 <template>
   <div class="container">
-    <!-- App组件是Category组件的使用者 -->
-    <Category title="游戏">
-      <template scope="obj">
-        <ul>
-          <li v-for="(item,index) in obj.games" :key="index">{{item}}</li>
-        </ul>
-      </template>
-    </Category>
-
-    <!-- 以下是解构赋值的写法 ES6中的语法 -->
-    <Category>
-      <template scope="{games}">
-        <ol>
-          <li v-for="(g,index) in games" :key="index">{{g}}</li>
-        </ol>
-      </template>
-    </Category>
-
-    <!-- template中的另一种写法 -->
-    <Category>
-      <template slot-scope="{games}">
-        <h4 v-for="(g,index) in games" :key="index">{{g}}</h4>
-      </template>
-    </Category>
+    <Count></Count>
   </div>
 </template>
 
 <script>
-import Category from './components/Category.vue'
+import Count from './components/Count'
+
 export default {
   components:{
-    Category
+    Count
   }
 }
 </script>
 
 <style>
-  .container,.link{
-    display: flex;
-    justify-content: space-around;
-  }
-  video{
-    width: 100%;
-  }
-  h4{
-    text-align: center;
-  }
+
 </style>
