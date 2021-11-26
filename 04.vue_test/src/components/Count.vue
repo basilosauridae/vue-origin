cca702ff6604a2cfa5c73b1f3147920662615cf9
New Author <2369700690@qq.com>
2021-11-26 13:56:35 +0800

    mapState和mapGetters的应用

--- 04.vue_test/src/components/Count.vue
+++ 04.vue_test/src/components/Count.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h3>当前求和为{{$store.state.sum}}</h3>
-    <h2>当前求和放大10倍为：{{$store.getters.bigSum}}</h2>
+    <h2>当前求和放大10倍为：{{bigSum}}</h2>
+    <p>我在{{address}}吃{{food}}</p>
     <select v-model.number="num">
       <!-- :value后是表达式是 会被解析成数字处理，如果不加:就变成字符串拼接了 |  方法二直接.number -->
       <option value="1">1</option>
@@ -14,14 +15,30 @@
     <button @click="incrementWait">等一等再加</button>
   </div>
 </template>
-
+ 
 <script>
+import {mapGetters, mapState} from 'vuex'
 export default {
   name:'Count', 
   data() {
     return {
       num:1,//用户选择的数字 
     }
+  },
+  computed:{
+    /* bigSum(){
+      return this.$store.getters.bigSum
+    }, */
+    // 借助mapState生成计算属性，从state中读取数据,不隐藏的是简写形式(数组写法)
+    /* ...mapState({
+      address:'address',
+      food:'food'
+    }) */
+    ...mapState(['address','food']),
+    /* ...mapGetters({
+      bigSum:'bigSum'
+    }) */
+    ...mapGetters(['bigSum']) //借助mapGetters生成计算属性，从getters中读取数据，简写（数组形式）
   },
   methods: {
     increment(){
