bd29dd345882f56b267f0bbb6ad5bd6c549d24d0
New Author <2369700690@qq.com>
2021-12-02 15:14:25 +0800

    嵌套（多级路由）

---
+++ 04.vue_test/src/pages/About.vue
@@ -0,0 +1,23 @@
+// pages文件夹中存放路由组件，components文件夹存放一般组件
+
+<template>
+  <div>
+    <h2>我是About内容</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'About',
+  beforeDestroy() {
+    console.log('About组件即将被摧毁')
+  },
+  mounted() {
+    console.log('About组件挂载完毕了')
+  },
+}
+</script>
+
+<style>
+
+</style>
