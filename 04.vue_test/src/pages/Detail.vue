0f3bde16f3053f3b3be105bf6ad5b03dfd42fb31
huying <huying@wondertek.com.cn>
2021-12-02 18:19:49 +0800

    路由的query

---
+++ 04.vue_test/src/pages/Detail.vue
@@ -0,0 +1,23 @@
+// 消息中的详情
+<template>
+  <div>
+    <ul>
+      <li>编号：{{$route.query.id}}</li>
+      <li>内容：{{$route.query.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list:this.$route.query
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
