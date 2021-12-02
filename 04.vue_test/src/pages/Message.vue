c715a806320b352b0b50067ed08054d0eab89bec
New Author <2369700690@qq.com>
2021-12-02 18:19:49 +0800

    路由的query

--- 04.vue_test/src/pages/Message.vue
+++ 04.vue_test/src/pages/Message.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
     <ul>
-      <li>
-        <a href="">message001</a> &nbsp;&nbsp;
-      </li>
-      <li>
-        <a href="">message002</a> &nbsp;&nbsp;
-      </li>
-      <li>
-        <a href="">message003</a> &nbsp;&nbsp;
+      <li v-for="item in list" :key="item.id">
+        <!-- 跳转路由并携带query参数的写法 to字符串的写法 -->
+        <!-- <router-link :to="`/home/message/detail?id=${item.id}&title=${item.msg}`">
+          {{item.msg}}
+        </router-link>  -->
+
+        <!-- 跳转路由并携带query参数的写法 to的对象写法 -->
+        <router-link :to="{
+          path:'/home/message/detail',
+          query:{
+            id:item.id,
+            title:item.msg
+          }
+        }">
+          {{item.msg}}
+        </router-link>
       </li>
     </ul>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name:'Message'
+  name:'Message',
+  data() {
+    return {
+      list:[
+        {id:"001",msg:'message001'},
+        {id:"002",msg:'message002'},
+        {id:"003",msg:'message003'}
+      ]
+    }
+  },
 }
 </script>
