9d5de0cd1982944885d899e0804b8449794c9624
New Author <2369700690@qq.com>
2021-12-14 10:37:35 +0800

    vue2的一些语法糖

---
+++ 05.vue2语法糖/3.拓展/03.todo-list/src/components/TodoFooter.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer class="footer">
+    <span class="todo-count">剩余 <strong>{{leftCount}}</strong> 个未完成</span>
+    <ul class="filters">
+      <li>
+        <a href="#/" @click="$emit('change-now', 'all')" :class="{selected:nowBtn==='all'}">全部</a>
+      </li>
+      <li>
+        <a href="#/active" @click="$emit('change-now', 'active')" :class="{selected:nowBtn==='active'}">未完成</a>
+      </li>
+      <li>
+        <a href="#/completed" @click="$emit('change-now', 'completed')" :class="{selected:nowBtn==='completed'}">已完成</a>
+      </li>
+    </ul>
+    <button class="clear-completed" @click="$emit('clear-todo')">清除已完成</button>
+  </footer>
+</template>
+
+<script>
+export default {
+  props:{
+    list:{
+      type:Array,
+      default:[]
+    },
+    nowBtn:{
+      type:String,
+    }
+  },
+  computed:{
+    leftCount(){
+      // 过滤出符合不等于当前flag即为false 未完成(剩余)的长度 
+      return  this.list.filter(item=>!item.flag).length
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 05.vue2语法糖/3.拓展/03.todo-list/src/components/TodoHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <h1>任务列表</h1>
+    <input class="new-todo" placeholder="请输入目标任务名称" @keyup.enter="add" v-model="todoName">
+  </header>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      todoName:''
+    }
+  },
+  methods:{
+    add(){
+      if(this.todoName.trim()==='')return
+      this.$emit('add-todo',this.todoName)
+      this.todoName = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
